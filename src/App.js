ed56ff4a7e09962504d1a50fbe05a849a69315f1
Victor <victor.du.77@hotmail.fr>
2020-04-10 12:17:22 +0200

    Add the equivalent of "if" in React

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
   }
 
   render() {
+    const won = new Date().getSeconds() % 2 === 0;
     return (
       <div className="memory">
         <GuessCount guesses={0} />
@@ -19,6 +20,7 @@ class App extends Component {
         <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
         <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
         <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+        {won && <p>Gagné !!</p>}
       </div>
     )
   }
